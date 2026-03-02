047f53f220e68d08f994ec501c18c03257992390
aleixmv10 <aleixmv10@alumnes.ilerna.com>
2026-03-02 19:06:49 +0100

    clase de gestor partida

--- src/CONTROLADOR/GestorJugador.java
+++ src/CONTROLADOR/GestorJugador.java
@@ -10,18 +10,35 @@ public class GestorJugador {
 		
 	}
 	public void jugadorSeMueve(Jugador j, int pasos, Tablero t) {
+	jugador.mover(pasos);
+	
+		System.out.println(jugador.getNombre() + 
+				" se mueve a la posición " + jugador.getPosicion());
 		
+				Casilla casillaActual = tablero.obtenerCasilla(jugador.getPosicion());
+
+        if (casillaActual != null) {
+            casillaActual.realizarAccion(null, jugador);
+		}
 	}
 	public void finalizaTurno(Jugador j) {
-		
+	 System.out.println("Finaliza el turno de " + jugador.getNombre());
 	}
 	public void pingüinoEvento(Pingüino p) {
-		
+	System.out.println(System.out.println("Evento especial para el pinguïno + p.getNombre"));
 	}
+
 	public void pingüinoGuerra(Pingüino p1, Pingüino p2) {
-		
+	System.out.println(p1.getNombre() + " entra en guerra con" + p2.getNombre);
+	
+	if(math.random()>0.5){
+		System.out.println(p1.getNombre() + "gana la guerra");
+	} else {
+		System.out.println(p2.getNombre() + "gana la guerra");
+	}
 	}
 	public void focaInteractua(Pingüino p, Foca f) {
-		
+		System.out.println("La doca interactúa con el pingüino" + p.getNombre)
+		f.setSomnoliento(true);
 	}
 }
--- src/CONTROLADOR/GestorPartida.java
+++ src/CONTROLADOR/GestorPartida.java
@@ -16,5 +16,26 @@ public class GestorPartida {
 		this.random = new Random();
 	}
 	
-
+	public GestorPartida() {
+		random = new Random();
+	}
+	
+	public void nuevaPartida(ArrayList<Jugador>jugadores, Tablero tablero) {
+	partida = new Partida(tablero, jugadores);
+	}
+	public int tirarDado (Dado dado){
+		return dado.tirar();
+	}
+	public void siguienteTurno() {
+		partida.siguienteTurno();
+	}
+	public void guardarPartida(){
+		gestorBBDD.guardarBBDD(partida);
+	}
+	public void cargarPartida(int id) {
+		partida = gestorBBDD.cargarBBDD(id);
+	}
+	public Partida getPartida(){
+		return partida;
+	}
 }
