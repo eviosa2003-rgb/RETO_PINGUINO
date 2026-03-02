dca6e1f05b2a9bf9853eb28188b9b7f67f24e54b
ericadmella <ericadmella@alumnes.ilerna.com>
2026-03-02 15:55:18 +0100

    Update Partida.java
    partida acabada

--- src/MODELO/Partida.java
+++ src/MODELO/Partida.java
@@ -2,4 +2,17 @@ package MODELO;
 
 public class Partida {
 
+    private Tablero tablero;
+    private ArrayList<Jugador> jugadores;
+    private int turnos;
+    private Jugador jugadorActual;
+    private boolean finalizada;
+    private Jugador ganador;
+
+    public Partida() {}
+
+    public Jugador getJugadorActual() {
+        return jugadorActual;
+    }
 }
+
