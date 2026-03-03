2461f71452aabbbdfca35b3be8cffddfbeb9c937
eviosa2003-rgb <eviosa2003@alumnes.ilerna.com>
2026-03-03 19:24:24 +0100

    he intentado hacer el tablero

--- src/MODELO/Tablero.java
+++ src/MODELO/Tablero.java
@@ -1,5 +1,41 @@
 package MODELO;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Tablero {
+	 private ArrayList<Casilla> casillas;
+	 private static final int TOTAL_CASILLAS = 50;
+	 
+	 public Tablero() {
+		 casillas = new ArrayList<>();
+		 generarCasillas();
+	 }
+	 
+	 private void generarCasillas() {
+		 Random r = new Random();
+		 for (int i = 0; i < TOTAL_CASILLAS; i++) {
+			 String tipo;
+			 
+			 if(i == 0 || i == TOTAL_CASILLAS - 1) {
+				 tipo = "NORMAL";
+			 }else {	               	       
+		          	int prob = r.nextInt(100);
 
+		          	if (prob < 10) tipo = "OSO";
+		          	else if (prob < 20) tipo = "FORAT";
+		          	else if (prob < 30) tipo = "TRINEU";
+		           	else if (prob < 40) tipo = "INTERROGANT";
+		           	else tipo = "NORMAL";
+		            }
+			 }
+		 }
+	 public Casilla getCasilla(int pos) {
+		 if(pos >= TOTAL_CASILLAS) pos = TOTAL_CASILLAS - 1;
+		 return casillas.get(pos);
+	 }
+	 
+	 public int getMeta() {
+		 return TOTAL_CASILLAS - 1;
+	 }
 }
+
